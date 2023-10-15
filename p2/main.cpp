c3a70288ac22b5388d0e8470f42a1bdfaf425696
SerhiiKarazinUni <serhii.lilikovych@student.karazin.ua>
2023-10-15 18:20:52 +0300

    Adds draft implementation for task 2

---
+++ p2/main.cpp
@@ -0,0 +1,242 @@
+using namespace std;
+
+#include <iostream>
+
+struct biguint{
+
+    uint64_t *data;
+    size_t size;
+
+    biguint() {
+        data = new uint64_t [0];
+        size = 0;
+    }
+
+    biguint(biguint* ref){
+        this->data = new uint64_t [ref->size];
+        memcpy(this->data, ref->data, sizeof(uint64_t)*ref->size);
+        this->size = ref->size;
+    }
+
+    biguint(const string hex){
+        if(hex.length() == 0){
+            data = new uint64_t [0];
+            size = 0;
+            return;
+        }
+
+        string _hex = string(hex);
+
+        if(hex.length()%2 == 1){
+            _hex += '0';
+        }
+
+        //adjust buffer length to fit the number
+        char sz = sizeof(uint64_t);
+        this->size = ceil(_hex.length()/2.0f/sz);
+        this->data = new uint64_t[this->size+1];
+
+        //read each byte into a temporary buffer
+        char* buffer = new char[sz];
+        unsigned int p = 0;
+        unsigned int k = 0;
+        for (unsigned int i = 0; i < _hex.length(); i += 2) {
+            string byteString = _hex.substr(i, 2);
+            buffer[p] = (char) strtol(byteString.c_str(), NULL, 16);
+            p++;
+
+            if(p == sz){
+                //need to push current buffer to struct data buffer
+                memcpy(&(data[k]), buffer, sz);
+                memset(buffer, 0 , sz);
+                k++;
+                p=0;
+            }
+        }
+
+        // if the temporary buffer is not empty -- flush it into the data buffer
+        if(p>0){
+            memcpy(&(data[k]), buffer, sz);
+        }
+    }
+};
+
+// thanks to:
+// https://stackoverflow.com/questions/10367616/bitwise-shifting-array-of-chars
+static void shift_bits(uint8_t *array, int len, int shift) {
+    bool left = false;
+
+    if(shift<0){
+        left = true;
+        shift *= -1;
+    }
+
+    uint8_t macro_shift = shift / 8;
+    shift = shift % 8;
+
+    uint8_t array_out[len];
+    memset(array_out, 0, len);
+
+    for(int i = 0; i < len; i++) {
+        if(left){
+            if (i + macro_shift < len)
+                array_out[i + macro_shift] += array[i] << shift;
+            if (i + macro_shift - 1 < len)
+                array_out[i + macro_shift - 1] += array[i] >> (8 - shift);
+        }else {
+            if (i + macro_shift < len)
+                array_out[i + macro_shift] += array[i] >> shift;
+            if (i + macro_shift + 1 < len)
+                array_out[i + macro_shift + 1] += array[i] << (8 - shift);
+        }
+    }
+
+    memcpy(array, array_out, len);
+}
+
+biguint operator << (biguint& src, const int shift) {
+    auto result = biguint(src);
+    shift_bits((uint8_t*)result.data, result.size*sizeof(uint64_t), -1*shift);
+    return result;
+}
+
+biguint operator >> (biguint& src, const int shift) {
+    auto result = biguint(src);
+    shift_bits((uint8_t*)result.data, result.size*sizeof(uint64_t), shift);
+    return result;
+}
+
+ostream& operator << (ostream& out, const biguint& num) {
+    char* buffer = new char[2];
+
+    for (char i = 0; i < num.size; i++) {
+        for(size_t k=0; k<sizeof(uint64_t); k++) {
+            printf("%.2x", ((unsigned char *) &num.data[i])[k]);
+            out << buffer;
+        }
+    }
+
+    return out;
+}
+
+biguint operator~(biguint& num){
+    biguint result = biguint(&num);
+
+    for (char i = 0; i < result.size; i++) {
+        result.data[i] = ~result.data[i];
+    }
+
+    return result;
+}
+
+biguint operator^(biguint& a, biguint& b){
+    biguint result;
+
+    if(a.size>b.size){
+        result = biguint(&a);
+    }else{
+        result = biguint(&b);
+    }
+
+    uint64_t zeros = 0;
+
+    for (char i = 0; i < result.size; i++) {
+        if(i<=a.size && i<=b.size)
+        {
+            result.data[i] = a.data[i] ^ b.data[i];
+        }
+        if(i<=a.size && i>b.size){
+            result.data[i] = a.data[i] ^ zeros;
+        }
+        if(i>a.size && i<=b.size){
+            result.data[i] = zeros ^ b.data[i];
+        }
+    }
+
+    return result;
+}
+
+biguint operator|(biguint& a, biguint& b){
+    biguint result;
+
+    if(a.size>b.size){
+        result = biguint(&a);
+    }else{
+        result = biguint(&b);
+    }
+
+    uint64_t zeros = 0;
+
+    for (char i = 0; i < result.size; i++) {
+        if(i<=a.size && i<=b.size)
+        {
+            result.data[i] = a.data[i] | b.data[i];
+        }
+        if(i<=a.size && i>b.size){
+            result.data[i] = a.data[i] | zeros;
+        }
+        if(i>a.size && i<=b.size){
+            result.data[i] = zeros | b.data[i];
+        }
+    }
+
+    return result;
+}
+
+biguint operator&(biguint& a, biguint& b){
+    biguint result;
+
+    if(a.size>b.size){
+        result = biguint(&a);
+    }else{
+        result = biguint(&b);
+    }
+
+    uint64_t zeros = 0;
+
+    for (char i = 0; i < result.size; i++) {
+        if(i<=a.size && i<=b.size)
+        {
+            result.data[i] = a.data[i] & b.data[i];
+        }
+        if(i<=a.size && i>b.size){
+            result.data[i] = a.data[i] & zeros;
+        }
+        if(i>a.size && i<=b.size){
+            result.data[i] = zeros & b.data[i];
+        }
+    }
+
+    return result;
+}
+
+
+int main() {
+    biguint numberA = biguint("FFFFFFFFFFFFFFFF0000000000000000");
+    biguint numberB = biguint("FF0000FF00000000000000FF00000000");
+    cout<<"getHex:\n";
+    cout<<"numberA: "<<numberA<<"\n";
+    cout<<"numberB: "<<numberB<<"\n\n";
+
+    cout<<"INV:\n";
+    cout<<"~numberA: "<<(~numberA)<<"\n";
+    cout<<"~numberB: "<<(~numberB)<<"\n\n";
+
+    cout<<"XOR:\n";
+    cout<<"numberA^numberB: "<<(numberA^numberB)<<"\n";
+    cout<<"should be      : 00ffff00ffffffff000000ff00000000\n\n";
+
+    cout<<"OR:\n";
+    cout<<"numberA|numberB: "<<(numberA|numberB)<<"\n";
+    cout<<"should be      : ffffffffffffffff000000ff00000000\n\n";
+
+    cout<<"AND:\n";
+    cout<<"numberA&numberB: "<<(numberA&numberB)<<"\n";
+    cout<<"should be      : ff0000ff000000000000000000000000\n\n";
+
+    cout<<"Bitwise shifting left and right 4 bits of numberA:\n";
+    cout<<"SRC: "<<numberA<<"\n";
+    cout<<"LFT: "<<(numberA<<4)<<"\n";
+    cout<<"RGT: "<<(numberA>>4)<<"\n";
+    return 0;
+}
